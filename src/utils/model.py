db250a026b88368958d38f9306aa422336ad0f77
Nitin-9 <er.nitintiwari548@gmail.com>
2022-01-30 14:46:09 +0530

    save model functionality added.

--- src/utils/model.py
+++ src/utils/model.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
+import time
+import os
 
 
 def create_model(LOSS_FUNCTION,OPTIMIZER,METRICS,NUM_CLASSES):
-    LAYERS=[
+    LAYERS = [
         tf.keras.layers.Flatten(input_shape=[28,28] ,name="inputlayer"),
         tf.keras.layers.Dense(300, activation="relu", name="hiddenlayer1"),
         tf.keras.layers.Dense(100, activation="relu", name="hiddenlayer2"),
@@ -12,3 +14,14 @@ def create_model(LOSS_FUNCTION,OPTIMIZER,METRICS,NUM_CLASSES):
     model_clf.compile(loss=LOSS_FUNCTION, optimizer=OPTIMIZER, metrics=METRICS)
 
     return model_clf ##<<< untrained model>>>
+
+
+def get_unique_filename(filename):
+    unique_filename = time.strftime(f"%Y%m%d_%H%M%S_{filename}")
+    return unique_filename
+
+
+def save_model(model, model_name, model_dir):
+    unique_filename = get_unique_filename(model_name)
+    path_to_model = os.path.join(model_dir, unique_filename)
+    model.save(path_to_model)
